1315af20a260f10ee30356db348cae34ae5ef64f
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-06-03 07:40:29 -0400

    Initial Content Settings

--- src/components/settings/ContentSettings.tsx
+++ src/components/settings/ContentSettings.tsx
@@ -1,70 +1,61 @@
+import { ReactSortable } from "react-sortablejs";
 import SettingsPage from "../SettingsPage";
+import { usePageSources } from "../../utilities/ContentPageContext";
+import Button from "../Button";
+import { css } from "@emotion/react";
 
 interface props {
-    currentlyShown: boolean;
+	currentlyShown: boolean;
 }
 
-function ContentSettings ({currentlyShown} : props) {
-    return (
-        <SettingsPage currentlyShown={currentlyShown}>
-            <h1>Content</h1>
-            {/* 
-             */}
-<p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p><p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p><p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p><p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-             <p>hi</p>
-        </SettingsPage>
-    )
+function ContentSettings({ currentlyShown }: props) {
+	const { pageSources, setPageSources } = usePageSources();
+
+	const style = css`
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
+	`;
+
+	const handleAddClick = () => {
+		const x = ["a", "b", "c"];
+
+		console.log(x.slice(-1)[0]);
+		setPageSources([
+			...pageSources,
+			{
+				id: (pageSources.slice(-1)[0]?.id ?? 0) + 1,
+				title: "",
+				sourceType: "",
+				source: "",
+			},
+		]);
+	};
+
+	pageSources.slice(-1);
+
+	return (
+		<SettingsPage currentlyShown={currentlyShown}>
+			<h1>Content</h1>
+			<div css={style}>
+				<h2>Title</h2>
+				<h2>Source Type</h2>
+				<h2>Source</h2>
+			</div>
+			<ReactSortable list={pageSources} setList={setPageSources}>
+				{pageSources.map((source) => (
+					<div css={style}>
+						<p>hi</p>
+						<p>hi</p>
+						<p>hi</p>
+					</div>
+				))}
+			</ReactSortable>
+
+			<Button onClick={handleAddClick}>
+				<p>Add Source</p>
+			</Button>
+		</SettingsPage>
+	);
 }
 
-export default ContentSettings
+export default ContentSettings;
