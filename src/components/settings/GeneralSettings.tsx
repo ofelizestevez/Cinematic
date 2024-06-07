0f65d35941f74d66f7106d9516cc15b6507210b8
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-06-07 16:38:55 -0400

    Load and Save Settings!!

--- src/components/settings/GeneralSettings.tsx
+++ src/components/settings/GeneralSettings.tsx
@@ -1,10 +1,53 @@
+import { css } from "@emotion/react";
 import SettingsPage from "../SettingsPage";
+import { ChangeEventHandler, useEffect, useState } from "react";
+import { LocalStorageKeys } from "../../utilities/LocalStorage";
+import { compressToBase64, decompressFromBase64} from "lz-string"
+import Input from "../simple/Input";
+import { useLoadSettings } from "../../hooks/useLoadSettings";
+const style = css`
+	> * {
+		margin-bottom: 1rem;
+	}
 
+	> * > div {
+		margin-bottom: 0.25rem;
+	}
+`;
 
 function GeneralSettings() {
+	const [compressedSettings, setCompressedSettings] = useState("")
+	const [saveSettingsString, setSaveSettingsString] = useState("")
+	const loadSettings = useLoadSettings();
+
+	useEffect(() => {
+		const settings = localStorage.getItem(LocalStorageKeys.settings)
+
+		setCompressedSettings(compressToBase64(settings?? ""))
+	})
+
+	const handleChange : ChangeEventHandler<HTMLInputElement> = (e) => {
+		setSaveSettingsString(e.target.value)
+	}
+	const handleClick = () => {
+		localStorage.setItem(LocalStorageKeys.settings, decompressFromBase64(saveSettingsString))
+		loadSettings()
+	}
+
 	return (
-		<SettingsPage>
+		<SettingsPage styles={style}>
 			<h1>General</h1>
+			<h2>Export Settings</h2>
+			<Input>
+				<input type="text" value={compressedSettings} />
+			</Input>
+			<h2>Import Settings</h2>
+			<Input>
+				<input type="text" value={saveSettingsString} onChange={handleChange}/>
+			</Input>
+			<Input>
+				<button onClick={handleClick}>Save</button>
+			</Input>
 		</SettingsPage>
 	);
 }
--- todo.md
+++ todo.md
@@ -1,19 +1,4 @@
-- [ ] Refactor Settings to SettingsSidebar and SettingsContent
-
-- [ ] Finish Content Settings Page
-- [ ] Add Content
-- [ ] Add Content Editing
-- [ ] Add Styling Editing
-
-- [ ] Finish Theme Settings Page
-- [ ] Add Custom Theme functionality
-  
-- [ ] Create a general settings page where the user can make a string that they can then copy to another computer and keep their same settings w/ lz-string
-
-
-
 ## Main To Do List 
 
-- [ ] Animate fade away of page indicator
-- [ ] animate fade in and fade out of content
+- [ ] Update settings after making a change
 
