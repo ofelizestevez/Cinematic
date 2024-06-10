68d3873fc74099124f743f177256085662c46c15
ofelizestevez <34465526+ofelizestevez@users.noreply.github.com>
2024-06-10 19:17:43 -0400

    starting fresh because I hate myself

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,8 @@
-// * Imports
-import { useEffect, useState } from "react";
-import {gsap} from "gsap";
-import { useGSAP } from "@gsap/react";
-
-
-import Content from "./components/Content";
-import Footer from "./components/Footer.tsx";
-import SettingsOverlay from "./components/SettingsOverlay.tsx";
-
-import { Theme, themeToObject } from "./utilities/Theme.ts";
-
-import { useSettings } from "./hooks/useSettings.ts";
-import { useLoadSettings } from "./hooks/useLoadSettings.ts";
-import { LocalStorageKeys } from "./utilities/LocalStorage.ts";
-import Header from "./components/Header.tsx";
-import useFetchPageData from "./hooks/useFetchPageData.tsx";
-
 // * Component
 function App() {
-    // * States
-	const [initialized, setInitialized] = useState(false)
-    const [theme, setTheme] = useState<Theme>(() => {
-		const savedTheme = localStorage.getItem(LocalStorageKeys.currentTheme);
-		return savedTheme ? (JSON.parse(savedTheme) as Theme) : Theme.LIGHT;
-	});
-
-    // * Hooks
-    const { showSettings, openSettings, closeSettings, settingsRef } = useSettings();
-    const loadSettings = useLoadSettings();
-    useFetchPageData(); // Use the custom hook to fetch page data
-    
-    // * Animates Theme
-    useGSAP(() => {
-		const themeVariableValues = themeToObject(theme);
-		gsap.to(":root", themeVariableValues);
-    }, [theme]);
-
-    // * App initialization
-    useEffect(() => {
-		if (!initialized) {
-            loadSettings();
-            setInitialized(true);
-        }
-    }, [initialized, loadSettings]);
 
     return (
         <>
-            <Header setTheme={setTheme} />
-            <Content />
-            {showSettings && <SettingsOverlay closeSettings={closeSettings} settingsRef={settingsRef} />}
-            <Footer openSettings={openSettings} />
         </>
     );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { ContentPageProvider } from "./hooks/ContentPageContext.tsx";
-import { PageDataProvider } from "./hooks/ContentPageDataContext.tsx";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-	ReactDOM.createRoot(rootElement).render(
-		<ContentPageProvider>
-			<PageDataProvider>
-				<App />
-			</PageDataProvider>
-		</ContentPageProvider>
-	);
+	ReactDOM.createRoot(rootElement).render(<App />);
 } else {
 	console.error("Root element not found");
 }
